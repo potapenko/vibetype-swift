e2d17a3394e8674b8fd6866d81bfbf0767220114
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2026-06-22 21:52:52 +0200

    Stop tracking reference source

--- references/openwhispr-main/src/components/ui/useSettingsLayout.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SettingsLayoutContext = React.createContext({ isCompact: false });
-
-export const SettingsLayoutProvider = SettingsLayoutContext.Provider;
-
-export const useSettingsLayout = () => React.useContext(SettingsLayoutContext);
